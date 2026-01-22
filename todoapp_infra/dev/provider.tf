fc929eb3dfa2e6fb17bc395e35174c63d5f391e5
Sajal-Rathore <94166065+Sajal-Rathore@users.noreply.github.com>
2026-01-22 16:24:12 +0530

    Update provider.tf

--- todoapp_infra/dev/provider.tf
+++ todoapp_infra/dev/provider.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "azurerm" {
   features {}
- subscription_id = "def13e29-f65a-496b-b10e-3cffca9a480f"
-tenant_id = "4b3a4a97-3e22-48f4-9f98-270e9a13692a"
+ subscription_id = "424e7f19-16c6-46e3-8fe3-4ccb32b686fd"
+tenant_id = "e30abb13-e248-442c-b835-29af747e86b6"
 
 }
 
